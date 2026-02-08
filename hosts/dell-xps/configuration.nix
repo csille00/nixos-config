444244b9e84ea223742a43d619d4958dfc0e0b38
connor <ellisconnor88@gmail.com>
2026-02-08 01:09:49 -0700

    add dell config

---
+++ hosts/dell-xps/configuration.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../shared/configuration.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "connor-dell";
+
+  programs.nm-applet.enable = true;
+
+  # Desktop environment
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.lxqt.enable = true;
+  environment.lxqt.excludePackages = with pkgs.lxqt; [ qterminal ];
+
+  users.users.ellisserver = {
+    isNormalUser = true;
+    description = "connor-dell";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  # Do not change - tracks initial NixOS install version for stateful data compatibility
+  system.stateVersion = "25.11";
+}

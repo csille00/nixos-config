c8d1e8a7b28588d4e35ef4075d4bf49d337af9e3
connor <ellisconnor88@gmail.com>
2026-02-22 16:16:25 -0700

    chinese drive mount point

--- hosts/ellis-server/configuration.nix
+++ hosts/ellis-server/configuration.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }:
 
+let
+  chinese-tb-drive-from-shopify = "06489CBC489CABC5";
+in
 {
   imports = [
     ./hardware-configuration.nix
@@ -26,6 +29,16 @@
     description = "ellisserver";
     extraGroups = [ "networkmanager" "wheel" ];
   };
+
+  # USB drive auto-mount
+  services.udev.extraRules = ''
+    ACTION=="add", ENV{ID_FS_UUID}=="${chinese-tb-drive-from-shopify}", RUN+="${pkgs.util-linux}/bin/mount -o uid=1000,gid=1000 /dev/%k /mnt/usb"
+    ACTION=="remove", ENV{ID_FS_UUID}=="${chinese-tb-drive-from-shopify}", RUN+="${pkgs.util-linux}/bin/umount /mnt/usb"
+  '';
+
+  systemd.tmpfiles.rules = [
+    "d /mnt/usb 0755 root root -"
+  ];
 
   # Do not change - tracks initial NixOS install version for stateful data compatibility
   system.stateVersion = "25.11";
